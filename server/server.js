63f840703f02178480219e9d9b9b5cb41f173c73
shashank8907 <shashank8907@hotmail.com>
2018-12-19 20:21:50 +0530

    add git ignore

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 import bodyParser from 'body-parser'
 import express from 'express'
+// add the path module
+import path from 'path'
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 const router = express.Router()
+
+// get reference to the client build directory
+const staticFiles = express.static(path.join(__dirname, '../../client/build'))  
+// pass the static files (react app) to the express app. 
+app.use(staticFiles)
+
+
 router.get('/cities', (req, res) => {
   const cities = [
     {name: 'New York City', population: 8175133},
@@ -13,7 +22,11 @@ router.get('/cities', (req, res) => {
   res.json(cities)
 })
 app.use(router)
+
+app.use('/*', staticFiles)
+
 app.set('port', (process.env.PORT || 3001))
+
 app.listen(app.get('port'), () => {
   console.log(`Listening on ${app.get('port')}`)
 })
